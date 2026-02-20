54a44f7ceca58fc805f4c63c522f03f420fc4b2e
Bo4a <116751025+El0Bosh5khep9ri9@users.noreply.github.com>
2026-02-20 19:04:13 +0200

    Update AppError.js

--- backend/src/utils/AppError.js
+++ backend/src/utils/AppError.js
@@ -3,19 +3,19 @@ class AppError extends Error {
     super(message);
 
     this.statusCode = statusCode;
-    this.status = `${statusCode}`.startsWith("4") ? "fail" : "error";
+    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
     this.isOperational = true;
 
     // --- التعديل هنا ---
     // إذا كنت لا تريد ظهور مسارات الملفات إطلاقاً، قم بحذف هذا السطر:
     // Error.captureStackTrace(this, this.constructor);
-
+    
     // الحل الأفضل: اجعل الـ stack يظهر فقط في مرحلة البرمجة (Development)
-    if (process.env.NODE_ENV === "development") {
+    if (process.env.NODE_ENV === 'development') {
       Error.captureStackTrace(this, this.constructor);
     } else {
       // في وضع الـ Production، نقوم بإخفاء الـ stack تماماً
-      this.stack = "";
+      this.stack = ''; 
     }
   }
 }
