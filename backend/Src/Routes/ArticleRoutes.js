c82482f94fb623e94c56bba52689bdf716ed0c96
Bo4a <116751025+El0Bosh5khep9ri9@users.noreply.github.com>
2026-04-10 05:40:57 +0200

    Create ArticleRoutes.js

---
+++ backend/Src/Routes/ArticleRoutes.js
@@ -0,0 +1,38 @@
+import express from "express";
+import * as articleController from "../Controllers/ArticleController.js";
+import * as authController from "../Controllers/AuthController.js";
+import uploadMiddleware from "../Middlewares/UploadMiddleware.js";
+import * as authMid from "../Middlewares/AuthMiddleware.js";
+
+
+const router = express.Router();
+
+// 1. عرض كل المقالات (للجميع - Public)
+router.get("/", articleController.getAllArticles);
+
+// --- كل المسارات القادمة تحتاج تسجيل دخول ---
+router.use(authMid.protect);
+
+// 2. الميزة الجديدة: جلب مقالات الدكاترة المتابعين فقط
+router.get("/following-feed", articleController.getFollowingArticles);
+
+// 3. إنشاء مقال جديد (للدكاترة فقط)
+router.post(
+    "/create",
+    authMid.restrictTo("doctor"),
+    uploadMiddleware.post,
+    articleController.createArticle
+);
+
+
+
+router.get(
+    "/my-articles", 
+    authMid.protect, 
+    authMid.restrictTo("doctor"), 
+    articleController.getMyArticles
+);
+
+
+
+export default router;
