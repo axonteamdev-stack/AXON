73cb2b79c2f6b839c3a53ba2d29b17b0a60d1143
Bo4a <116751025+El0Bosh5khep9ri9@users.noreply.github.com>
2026-04-13 09:48:37 +0200

    Update MedicationRoutes.js

--- backend/Src/Routes/MedicationRoutes.js
+++ backend/Src/Routes/MedicationRoutes.js
@@ -1,8 +1,8 @@
 import express from "express";
 import * as medController from "../Controllers/MedicationController.js";
 import { protect, restrictTo } from "../Middlewares/AuthMiddleware.js";
-import validateMiddleware from "../Middlewares/ValidateMiddleware.js";
-import uploadMiddleware from "../Middlewares/UploadMiddleware.js";
+import validateMiddleware from "../Middlewares/ValidateMiddleware.js"; // تأكد من المسار والاسم
+import { upload } from "../Middlewares/UploadMiddleware.js";
 
 const router = express.Router();
 
@@ -10,7 +10,7 @@ const router = express.Router();
 router.use(protect);
 
 // 2. قصر الوصول للمرضى فقط (لأن الأدوية خاصة بالمريض)
-router.use(restrictTo("patient"));
+router.use(restrictTo('patient'));
 
 // --- المسارات الأساسية ---
 
@@ -19,18 +19,32 @@ router.get("/", medController.getMyMedications);
 
 // إضافة دواء جديد
 router.post(
-  "/",
-  uploadMiddleware.none,
-  validateMiddleware.addMedication,
-  medController.addMedication,
+  "/", 
+  upload.none(), 
+  validateMiddleware.addMedication, 
+  medController.addMedication
 );
 
 // تحديث دواء معين بواسطة المعرف (ID)
-router.patch("/:id", uploadMiddleware.none, medController.updateMedication);
+router.patch(
+  "/:id", 
+  upload.none(), 
+  medController.updateMedication
+);
 
 // حذف دواء معين بواسطة المعرف (ID)
-router.delete("/:id", medController.deleteMedication);
+router.delete(
+  "/:id", 
+  medController.deleteMedication
+);
+
+
 
 router.get("/:id", medController.getSingleMedication);
+
+
+router.patch("/:id/taken", medController.markAsTaken);
+
+router.patch("/:id/skip", medController.skipDose);
 
 export default router;
