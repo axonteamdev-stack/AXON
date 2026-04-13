030cb3300a936417d578c3cd91240d33660cb5fe
Bo4a <116751025+El0Bosh5khep9ri9@users.noreply.github.com>
2026-04-13 09:42:13 +0200

    Update LanguageMiddleware.js

--- backend/Src/Middlewares/LanguageMiddleware.js
+++ backend/Src/Middlewares/LanguageMiddleware.js
@@ -1,11 +1,7 @@
 export const setLanguage = (req, res, next) => {
-  // Extracting the first 2 characters to handle 'en-US' or 'ar-EG'
-  let lang = req.headers["accept-language"]?.substring(0, 2).toLowerCase();
-  const supportedLanguages = ["en", "ar"];
-
-  // Default to 'ar' (Arabic) as primary language if not provided or supported
-  // This ensures consistency with error handlers and API design
-  req.lang = supportedLanguages.includes(lang) ? lang : "ar";
-
-  next();
+    // الموبايل هيبعت في الهيدر 'ar' أو 'en'
+    // لو مبعتش حاجة، هنخلي الافتراضي 'ar'
+    const lang = req.headers['language'] === 'en' ? 'en' : 'ar';
+    req.lang = lang; 
+    next();
 };
